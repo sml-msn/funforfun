4c0ca2bea45772825ee6dd3d75ff4f5d9f0b9277
sml-msn <121129280+sml-msn@users.noreply.github.com>
2023-01-10 20:03:56 +0500

    Update kkkk.py

--- kkkk.py
+++ kkkk.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-
+import pandas as pd
+from Levenshtein import distance, ratio
 
 model_name = "sml-msn/pst5-tg-fa-bidirectional"
 tokenizer = T5Tokenizer.from_pretrained(model_name)
@@ -41,6 +42,33 @@ def split_n_translit(txt):
 		st.write(' '.join(text))
 		return True
 
+def demo():
+	sample = pd.read_csv('test180k.csv', sep=',').sample(1)
+	
+	# tg-fa
+	for k, row in sample.iterrows():
+		target = row.Pers
+		prediction = translit(row.Taj, max_length = 1024)
+		st.write('input:', row.Taj)
+		st.write('target:', target)
+		st.write('prediction:', prediction)
+		st.write('lev. distance:', distance(target, prediction))
+		st.write('lev. ratio:', ratio(target, prediction))
+	
+	st.write()
+	st.write('--------------------------------------------')
+	st.write()	
+	
+	# fa-tg
+	for k, row in sample.iterrows():
+		target = row.Taj
+		prediction = translit(row.Pers, max_length = 1024)
+		st.write('input:', row.Pers)
+		st.write('target:', target)
+		st.write('prediction:', prediction)
+		st.write('lev. distance:', distance(target, prediction))
+		st.write('lev. ratio:', ratio(target, prediction))	
+	
 st.title('Tg-Fa transliterator')
 
 txt = st.text_area('Enter your text:', placeholder = 'Your text must have less than 500 symbols.')
